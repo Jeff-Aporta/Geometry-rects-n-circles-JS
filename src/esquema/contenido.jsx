fde131237b165e849ff5b0afa11d65b11e4d2093
Jeffrey Agudelo <jeffaporta@gmail.com>
2024-07-31 08:58:24 -0500

    Geometry-rects-n-circles-JS

---
+++ src/esquema/contenido.jsx
@@ -0,0 +1,30 @@
+const _GEOMETRY_RECTS_N_CIRCLES_ = new GenerarContenidoLibreria({
+    nombre: 'Geometry Rects n Circles JS',
+    slogan: 'Geometría Dinámica: Simplifica tus Colisiones y Transformaciones',
+    img: 'src/img/logo.jpeg',
+
+    github: "https://github.com/Jeff-Aporta/Geometry-rects-n-circles-JS",
+
+    resumen: {
+        desc: `
+            *Geometry Rects n Circles JS*. es una librería básica diseñada para manejar colisiones, especialmente en proyectos con p5.js. 
+            Permite trabajar con rectángulos y círculos, facilitando su movimiento, redimensionamiento y cálculo de colisiones, 
+            uniones e intersecciones. Ideal para crear hitboxes en juegos básicos, la librería incluye clases y funciones para 
+            manipular gráficos de manera sencilla.
+        `,
+        descImg: [
+            "Interpretar un número telefónico y determinar su país de origen",
+            "Extraer números telefónicos de un texto",
+            "Usa la API 'https://flagicons.lipis.dev/flags/' para mostrar la bandera del país"
+        ]
+    },
+
+    secciones: [
+        {
+            nombre: "seccion 1",
+            contenido: (thisObj) => {
+                return "contenido de la seccion 1";
+            }
+        }
+    ]
+});
